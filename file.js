9ade280791d16cf606142f87052fd5c5ad03b937
Leonardo Dias <leonardodias@ucl.br>
2021-06-30 14:41:12 -0300

    Finish desktop Css design and implement Mobile Menu function

--- file.js
+++ file.js
@@ -1,3 +1,15 @@
+const hamburguer = document.getElementById('hamburger-btn');
+const mobMenu = document.getElementById('mobile_menu');
+
 function myFunction(x) {
     x.classList.toggle("change");
+    if (mobMenu.style.visibility == "visible") {
+      mobMenu.style.visibility = "hidden";
+      mobMenu.style.height = "0";
+      mobMenu.style.overflowY = "hidden";
+    } else {
+      mobMenu.style.visibility = "visible";
+      mobMenu.style.height = "100vh";
+      mobMenu.style.overflowY = "auto";
+    }
   }
